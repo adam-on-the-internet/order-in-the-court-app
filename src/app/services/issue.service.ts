5400859ad80508d4119e146f42a7d2fd9453e609
Adam Callanan <a.callanan@sbcglobal.net>
2020-02-18 22:52:35 -0600

    services completed

--- src/app/services/issue.service.ts
+++ src/app/services/issue.service.ts
@@ -1,9 +1,60 @@
 import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { Issue } from "../models/Issue.model";
+import { RestUrlBuilder } from "../utilities/rest-url-builder.util";
+import { ServiceUrl } from "../constants/rest.constants";
+import { CookieHelper } from "../utilities/cookie.util";
+
+const controller = "issue";
 
 @Injectable({
   providedIn: "root"
 })
 export class IssueService {
+  constructor(
+    private http: HttpClient,
+  ) { }
 
-  constructor() { }
+  public getIssue(): Observable<Issue[]> {
+    const url = RestUrlBuilder.buildRestUrl({
+      service: ServiceUrl.BasicExpress,
+      controller
+    });
+    return this.http.get(url) as Observable<Issue[]>;
+  }
+
+  public getSingleIssue(issueId: string): Observable<Issue> {
+    const url = RestUrlBuilder.buildRestUrl({
+      service: ServiceUrl.BasicExpress,
+      controller,
+      params: issueId,
+    });
+    return this.http.get(url) as Observable<Issue>;
+  }
+
+  public createIssue(issue: any): Observable<any> {
+    const url = RestUrlBuilder.buildRestUrl({
+      service: ServiceUrl.BasicExpress,
+      controller
+    });
+    return this.http.post(url, issue, CookieHelper.authHeaders) as Observable<any>;
+  }
+
+  public updateIssue(issue: any): Observable<any> {
+    const url = RestUrlBuilder.buildRestUrl({
+      service: ServiceUrl.BasicExpress,
+      controller
+    });
+    return this.http.put(url, issue, CookieHelper.authHeaders) as Observable<any>;
+  }
+
+  public deleteIssue(issueId: string): Observable<any> {
+    const url = RestUrlBuilder.buildRestUrl({
+      service: ServiceUrl.BasicExpress,
+      controller,
+      params: issueId,
+    });
+    return this.http.delete(url, CookieHelper.authHeaders) as Observable<any>;
+  }
 }
--- src/app/services/witness.service.ts
+++ src/app/services/witness.service.ts
@@ -1,9 +1,60 @@
 import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { RestUrlBuilder } from "../utilities/rest-url-builder.util";
+import { Observable } from "rxjs";
+import { ServiceUrl } from "../constants/rest.constants";
+import { Witness } from "../models/Witness.model";
+import { CookieHelper } from "../utilities/cookie.util";
+
+const controller = "witness";
 
 @Injectable({
   providedIn: "root"
 })
 export class WitnessService {
+  constructor(
+    private http: HttpClient,
+  ) { }
 
-  constructor() { }
+  public getWitness(): Observable<Witness[]> {
+    const url = RestUrlBuilder.buildRestUrl({
+      service: ServiceUrl.BasicExpress,
+      controller
+    });
+    return this.http.get(url) as Observable<Witness[]>;
+  }
+
+  public getSingleWitness(witnessId: string): Observable<Witness> {
+    const url = RestUrlBuilder.buildRestUrl({
+      service: ServiceUrl.BasicExpress,
+      controller,
+      params: witnessId,
+    });
+    return this.http.get(url) as Observable<Witness>;
+  }
+
+  public createWitness(witness: any): Observable<any> {
+    const url = RestUrlBuilder.buildRestUrl({
+      service: ServiceUrl.BasicExpress,
+      controller
+    });
+    return this.http.post(url, witness, CookieHelper.authHeaders) as Observable<any>;
+  }
+
+  public updateWitness(witness: any): Observable<any> {
+    const url = RestUrlBuilder.buildRestUrl({
+      service: ServiceUrl.BasicExpress,
+      controller
+    });
+    return this.http.put(url, witness, CookieHelper.authHeaders) as Observable<any>;
+  }
+
+  public deleteWitness(witnessId: string): Observable<any> {
+    const url = RestUrlBuilder.buildRestUrl({
+      service: ServiceUrl.BasicExpress,
+      controller,
+      params: witnessId,
+    });
+    return this.http.delete(url, CookieHelper.authHeaders) as Observable<any>;
+  }
 }
