8b74051663e093dfab7d2515967e3ff199052015
Adam Callanan <a.callanan@sbcglobal.net>
2020-02-18 23:05:44 -0600

    issue form

--- src/app/components/issue-form/issue-form.component.ts
+++ src/app/components/issue-form/issue-form.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from "@angular/core";
+import { Issue } from 'src/app/models/Issue.model';
+import { BooleanHelper } from 'src/app/utilities/boolean.util';
+import { ActivatedRoute } from '@angular/router';
+import { IssueService } from 'src/app/services/issue.service';
+import { NavHelperService } from 'src/app/services/nav-helper.service';
 
 @Component({
   selector: "app-issue-form",
@@ -6,10 +11,103 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./issue-form.component.css"]
 })
 export class IssueFormComponent implements OnInit {
+  public issue: Issue = null;
+  public showErrors = false;
 
-  constructor() { }
+  public get errors(): string[] {
+    const myErrors = [];
+    if (this.nameInvalid) {
+      myErrors.push("Please provide a name.");
+    }
+    return myErrors;
+  }
 
-  ngOnInit() {
+  public get valid(): boolean {
+    return this.errors.length === 0;
+  }
+
+  public get ready(): boolean {
+    return BooleanHelper.hasValue(this.issue);
+  }
+
+  public get editMode(): boolean {
+    return this.ready && BooleanHelper.hasValue(this.issue._id);
+  }
+
+  private get nameInvalid(): boolean {
+    return !BooleanHelper.hasValue(this.issue.name);
+  }
+
+  constructor(
+    private route: ActivatedRoute,
+    private issueService: IssueService,
+    private navHelper: NavHelperService,
+  ) { }
+
+  public ngOnInit() {
+    this.setupForm();
+  }
+
+  public submit() {
+    this.showErrors = true;
+    if (this.valid) {
+      this.save();
+    }
+  }
+
+  private save() {
+    if (this.editMode) {
+      this.runEdit();
+    } else {
+      this.runCreate();
+    }
+  }
+
+  private runEdit() {
+    let response;
+    this.issueService.updateIssue(this.issue)
+      .subscribe((res) => response = res,
+        (error) => {
+          console.log("update issue failed");
+        }, () => {
+          this.navHelper.goToIssueOverview();
+        });
+  }
+
+  private runCreate() {
+    let response;
+    this.issueService.createIssue(this.issue)
+      .subscribe((res) => response = res,
+        (error) => {
+          console.log("create issue failed");
+        }, () => {
+          this.navHelper.goToIssueOverview();
+        });
+  }
+
+  private setupForm() {
+    const urlId = this.route.snapshot.paramMap.get("id");
+    if (BooleanHelper.notNull(urlId)) {
+      this.setupEditForm(urlId);
+    } else {
+      this.setupCreateForm();
+    }
+  }
+
+  private setupEditForm(id: string): void {
+    this.issueService.getSingleIssue(id)
+      .subscribe((res) => this.issue = res,
+        (error) => {
+          console.log("get issue failed");
+        });
+  }
+
+  private setupCreateForm(): void {
+    this.issue = {
+      name: null,
+      description: null,
+      _id: null,
+    };
   }
 
 }
