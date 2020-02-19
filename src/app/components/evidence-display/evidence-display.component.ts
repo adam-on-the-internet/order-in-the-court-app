6cd30f3e936e8737648d5b0bfc7d7de8f2d2d51f
Adam Callanan <a.callanan@sbcglobal.net>
2020-02-18 22:35:49 -0600

    evidence display

--- src/app/components/evidence-display/evidence-display.component.ts
+++ src/app/components/evidence-display/evidence-display.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from "@angular/core";
+import { Evidence } from 'src/app/models/Evidence.model';
+import { BooleanHelper } from 'src/app/utilities/boolean.util';
+import { ActivatedRoute } from '@angular/router';
+import { EvidenceService } from 'src/app/services/evidence.service';
 
 @Component({
   selector: "app-evidence-display",
@@ -6,10 +10,28 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./evidence-display.component.css"]
 })
 export class EvidenceDisplayComponent implements OnInit {
+  public evidence: Evidence = null;
 
-  constructor() { }
+  public get ready(): boolean {
+    return BooleanHelper.notNull(this.evidence);
+  }
 
-  ngOnInit() {
+  constructor(
+    private evidenceService: EvidenceService,
+    private route: ActivatedRoute,
+  ) { }
+
+  public ngOnInit() {
+    this.loadContraption();
+  }
+
+  private loadContraption(): void {
+    const id = this.route.snapshot.paramMap.get("id");
+    this.evidenceService.getSingleEvidence(id)
+      .subscribe((res) => this.evidence = res,
+        (error) => {
+          console.log("get evidence failed");
+        });
   }
 
 }
