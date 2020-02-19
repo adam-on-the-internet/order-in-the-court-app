795b618520508407dc386a6beb4fb642ea52a38b
Adam Callanan <a.callanan@sbcglobal.net>
2020-02-18 22:59:03 -0600

    overview and table setup

--- src/app/components/issue-table/issue-table.component.ts
+++ src/app/components/issue-table/issue-table.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from "@angular/core";
+import { Issue } from 'src/app/models/Issue.model';
+import { CookieHelper } from 'src/app/utilities/cookie.util';
+import { IssueService } from 'src/app/services/issue.service';
+import { NavHelperService } from 'src/app/services/nav-helper.service';
 
 @Component({
   selector: "app-issue-table",
@@ -6,10 +10,65 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./issue-table.component.css"]
 })
 export class IssueTableComponent implements OnInit {
+  public issues: Issue[] = null;
+  public error = false;
 
-  constructor() { }
-
-  ngOnInit() {
+  public get canEdit(): boolean {
+    return CookieHelper.admin;
   }
+
+  public get ready(): boolean {
+    return this.issues !== null;
+  }
+
+  constructor(
+    private issueService: IssueService,
+    private navHelper: NavHelperService,
+  ) { }
+
+  public ngOnInit() {
+    this.loadIssues();
+  }
+
+  public issueDetails(issue: Issue): void {
+    this.navHelper.goToIssueDetails(issue._id);
+  }
+
+  public createIssue(): void {
+    this.navHelper.goToIssueCreateForm();
+  }
+
+  public editIssue(issue: Issue): void {
+    this.navHelper.goToIssueEditForm(issue._id);
+  }
+
+  public requestDelete(issue: Issue): void {
+    const confirmDelete = confirm(`Are you sure you want to delete ${issue.name}?`);
+    if (confirmDelete) {
+      this.deleteIssue(issue);
+    }
+  }
+
+  private deleteIssue(issue: Issue): void {
+    this.issueService.deleteIssue(issue._id)
+      .subscribe((res) => this.issues = res,
+        (error) => {
+          console.log("delete issue failed");
+        }, () => {
+          this.loadIssues();
+        });
+  }
+
+  private loadIssues(): void {
+    this.issues = null;
+    this.error = false;
+    this.issueService.getIssue()
+      .subscribe((res) => this.issues = res,
+        (error) => {
+          this.error = true;
+          console.log("get issues failed");
+        });
+  }
+
 
 }
--- src/app/components/witness-table/witness-table.component.ts
+++ src/app/components/witness-table/witness-table.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from "@angular/core";
+import { Witness } from 'src/app/models/Witness.model';
+import { CookieHelper } from 'src/app/utilities/cookie.util';
+import { WitnessService } from 'src/app/services/witness.service';
+import { NavHelperService } from 'src/app/services/nav-helper.service';
 
 @Component({
   selector: "app-witness-table",
@@ -6,10 +10,65 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./witness-table.component.css"]
 })
 export class WitnessTableComponent implements OnInit {
+  public witnesses: Witness[] = null;
+  public error = false;
 
-  constructor() { }
-
-  ngOnInit() {
+  public get canEdit(): boolean {
+    return CookieHelper.admin;
   }
+
+  public get ready(): boolean {
+    return this.witnesses !== null;
+  }
+
+  constructor(
+    private witnessService: WitnessService,
+    private navHelper: NavHelperService,
+  ) { }
+
+  public ngOnInit() {
+    this.loadWitnesss();
+  }
+
+  public witnessDetails(witness: Witness): void {
+    this.navHelper.goToWitnessDetails(witness._id);
+  }
+
+  public createWitness(): void {
+    this.navHelper.goToWitnessCreateForm();
+  }
+
+  public editWitness(witness: Witness): void {
+    this.navHelper.goToWitnessEditForm(witness._id);
+  }
+
+  public requestDelete(witness: Witness): void {
+    const confirmDelete = confirm(`Are you sure you want to delete ${witness.name}?`);
+    if (confirmDelete) {
+      this.deleteWitness(witness);
+    }
+  }
+
+  private deleteWitness(witness: Witness): void {
+    this.witnessService.deleteWitness(witness._id)
+      .subscribe((res) => this.witnesses = res,
+        (error) => {
+          console.log("delete witness failed");
+        }, () => {
+          this.loadWitnesss();
+        });
+  }
+
+  private loadWitnesss(): void {
+    this.witnesses = null;
+    this.error = false;
+    this.witnessService.getWitness()
+      .subscribe((res) => this.witnesses = res,
+        (error) => {
+          this.error = true;
+          console.log("get witnesss failed");
+        });
+  }
+
 
 }
