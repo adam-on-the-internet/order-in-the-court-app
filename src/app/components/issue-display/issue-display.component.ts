0b296cd6f544c224d3678dbe908cec0e55ae7987
Adam Callanan <a.callanan@sbcglobal.net>
2020-02-18 23:02:46 -0600

    details

--- src/app/components/issue-display/issue-display.component.ts
+++ src/app/components/issue-display/issue-display.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from "@angular/core";
+import { Issue } from 'src/app/models/Issue.model';
+import { BooleanHelper } from 'src/app/utilities/boolean.util';
+import { ActivatedRoute } from '@angular/router';
+import { IssueService } from 'src/app/services/issue.service';
 
 @Component({
   selector: "app-issue-display",
@@ -6,10 +10,28 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./issue-display.component.css"]
 })
 export class IssueDisplayComponent implements OnInit {
+  public issue: Issue = null;
 
-  constructor() { }
+  public get ready(): boolean {
+    return BooleanHelper.notNull(this.issue);
+  }
 
-  ngOnInit() {
+  constructor(
+    private issueService: IssueService,
+    private route: ActivatedRoute,
+  ) { }
+
+  public ngOnInit() {
+    this.loadContraption();
+  }
+
+  private loadContraption(): void {
+    const id = this.route.snapshot.paramMap.get("id");
+    this.issueService.getSingleIssue(id)
+      .subscribe((res) => this.issue = res,
+        (error) => {
+          console.log("get issue failed");
+        });
   }
 
 }
--- src/app/components/witness-display/witness-display.component.ts
+++ src/app/components/witness-display/witness-display.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from "@angular/core";
+import { Witness } from 'src/app/models/Witness.model';
+import { BooleanHelper } from 'src/app/utilities/boolean.util';
+import { WitnessService } from 'src/app/services/witness.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: "app-witness-display",
@@ -6,10 +10,28 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./witness-display.component.css"]
 })
 export class WitnessDisplayComponent implements OnInit {
+  public witness: Witness = null;
 
-  constructor() { }
+  public get ready(): boolean {
+    return BooleanHelper.notNull(this.witness);
+  }
 
-  ngOnInit() {
+  constructor(
+    private witnessService: WitnessService,
+    private route: ActivatedRoute,
+  ) { }
+
+  public ngOnInit() {
+    this.loadContraption();
+  }
+
+  private loadContraption(): void {
+    const id = this.route.snapshot.paramMap.get("id");
+    this.witnessService.getSingleWitness(id)
+      .subscribe((res) => this.witness = res,
+        (error) => {
+          console.log("get witness failed");
+        });
   }
 
 }
