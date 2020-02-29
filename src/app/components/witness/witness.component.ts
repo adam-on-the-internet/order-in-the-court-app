9198899918b5f062a001ca3176acb3d97db9061a
Adam Callanan <a.callanan@sbcglobal.net>
2020-02-28 23:42:23 -0600

    reveal witness

--- src/app/components/witness/witness.component.ts
+++ src/app/components/witness/witness.component.ts
@@ -3,6 +3,7 @@ import { BooleanHelper } from "src/app/utilities/boolean.util";
 import { CaseService } from "src/app/services/case.service";
 import { ActivatedRoute } from "@angular/router";
 import { Case } from "src/app/models/Case.model";
+import { Witness } from 'src/app/models/Witness.model';
 
 @Component({
   selector: "app-witness",
@@ -24,6 +25,15 @@ export class WitnessComponent implements OnInit {
 
   public ngOnInit() {
     this.loadCase();
+  }
+
+  public revealWitness(witness: Witness) {
+    this.caseService.revealWitness(this.case._id, witness._id)
+      .subscribe((res) => this.case = res,
+        (error) => {
+          this.error = true;
+          console.log("reveal witness failed");
+        });
   }
 
   private loadCase() {
