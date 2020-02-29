5f45a8672444dc9e8e424581b35f51a97400043c
Adam Callanan <a.callanan@sbcglobal.net>
2020-02-29 00:48:29 -0600

    adjust e2e

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe("workspace-project App", () => {
 
   it("should display welcome message", () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual("order-in-the-court-app");
+    expect(page.getTitleText()).toEqual("Order in the Court");
   });
 
   afterEach(async () => {
