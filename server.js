a7635e46d6c1b373ea1689067e6172909726ec87
Adam Callanan <a.callanan@sbcglobal.net>
2020-03-14 22:39:26 -0500

    enforce https

--- server.js
+++ server.js
@@ -4,6 +4,19 @@ const path = require('path');
 
 const app = express();
 
+const forceSSL = function () {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+        ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
+
+app.use(forceSSL());
+
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/order-in-the-court-app'));
 
