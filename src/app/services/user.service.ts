77d1b137118928edaf6bf777b4a1ba14749b0e49
Adam Callanan <a.callanan@sbcglobal.net>
2020-04-25 13:54:12 -0500

    remove unused functionality

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -3,8 +3,6 @@ import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { RestUrlBuilder } from "../utilities/rest-url-builder.util";
 import { ServiceUrl } from "../constants/rest.constants";
-import { CookieHelper } from "../utilities/cookie.util";
-import { User } from "../models/User.model";
 
 @Injectable({
   providedIn: "root"
@@ -13,14 +11,6 @@ export class UserService {
   constructor(
     private http: HttpClient,
   ) { }
-
-  public getAllUsers(): Observable<User[]> {
-    const url = RestUrlBuilder.buildRestUrl({
-      service: ServiceUrl.BasicExpress,
-      controller: "user"
-    });
-    return this.http.get(url, CookieHelper.authHeaders) as Observable<User[]>;
-  }
 
   public register(email: string): Observable<any> {
     const url = RestUrlBuilder.buildRestUrl({
@@ -34,44 +24,6 @@ export class UserService {
     return this.http.post(url, user);
   }
 
-  public updateEmailSelf(email: string): Observable<any> {
-    const url = RestUrlBuilder.buildRestUrl({
-      service: ServiceUrl.BasicExpress,
-      controller: "user",
-      collection: "emailUpdate/self"
-    });
-    const user = {
-      newEmail: email
-    };
-    return this.http.put(url, user, CookieHelper.authHeaders);
-  }
-
-  public editAccess(email: string, specialAccess: string): Observable<any> {
-    const url = RestUrlBuilder.buildRestUrl({
-      service: ServiceUrl.BasicExpress,
-      controller: "user",
-      collection: "specialAccess"
-    });
-    const body = {
-      email,
-      specialAccess
-    };
-    return this.http.put(url, body, CookieHelper.authHeaders);
-  }
-
-  public setAdmin(email: string, isAdmin: boolean): Observable<any> {
-    const url = RestUrlBuilder.buildRestUrl({
-      service: ServiceUrl.BasicExpress,
-      controller: "user",
-      collection: "setAdmin"
-    });
-    const body = {
-      email,
-      isAdmin
-    };
-    return this.http.put(url, body, CookieHelper.authHeaders);
-  }
-
   public resetPasswordAutomatic(email: string): Observable<any> {
     const url = RestUrlBuilder.buildRestUrl({
       service: ServiceUrl.BasicExpress,
@@ -82,27 +34,5 @@ export class UserService {
       email
     };
     return this.http.put(url, user);
-  }
-
-  public resetPasswordManual(password: string, confirmPassword: string): Observable<any> {
-    const url = RestUrlBuilder.buildRestUrl({
-      service: ServiceUrl.BasicExpress,
-      controller: "user",
-      collection: "passwordReset/manual"
-    });
-    const body = {
-      password,
-      confirmPassword
-    };
-    return this.http.put(url, body, CookieHelper.authHeaders);
-  }
-
-  public deleteUser(id: string): Observable<any> {
-    const url = RestUrlBuilder.buildRestUrl({
-      service: ServiceUrl.BasicExpress,
-      controller: "user",
-      collection: id
-    });
-    return this.http.delete(url, CookieHelper.authHeaders);
   }
 }
