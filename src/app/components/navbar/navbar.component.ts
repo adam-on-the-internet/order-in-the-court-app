61c00b9ac230b15386a01a68f69aec4b896bc352
Adam Callanan <a.callanan@sbcglobal.net>
2020-05-28 22:28:43 -0500

    take case out of navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,8 +1,6 @@
 import {Component} from "@angular/core";
 import {NavHelperService} from "src/app/services/nav-helper.service";
 import {CookieHelper} from "src/app/utilities/cookie.util";
-import {CaseManagerService} from "../../services/case-manager.service";
-import {IdentityService} from "../../services/identity.service";
 import {EnvUtil} from "../../utilities/env.util";
 
 @Component({
@@ -20,31 +18,17 @@ export class NavbarComponent {
     return CookieHelper.getUserDetails().admin;
   }
 
-  public get showCase(): boolean {
-    return this.caseManagerService.caseReady && !this.caseManagerService.activeCase.isClosed;
-  }
-
-  public get caseName(): string {
-    return this.caseManagerService.activeCase.fullCaseName;
-  }
-
   public get nonprod(): boolean {
     return EnvUtil.usingNonprod;
   }
 
   constructor(
     private navHelper: NavHelperService,
-    private identityManager: IdentityService,
-    private caseManagerService: CaseManagerService,
   ) {
   }
 
   public goToDashboard(): void {
     this.navHelper.goToDashboard();
-  }
-
-  public goToCase(): void {
-    this.navHelper.join(this.caseManagerService.activeCase._id);
   }
 
   public goToJoin(): void {
