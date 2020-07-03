c6024761de448bc19726ae0798d9ea8bc3a538d8
Adam Callanan <a.callanan@sbcglobal.net>
2020-07-03 14:48:08 -0500

    hide prompt if accessing archive

--- src/app/components/case-archive/case-archive.component.ts
+++ src/app/components/case-archive/case-archive.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from "@angular/core";
 import {BooleanHelper} from "../../utilities/boolean.util";
 import {CaseService} from "../../services/case.service";
 import {Case} from "../../models/Case.model";
+import {ContactService} from "../../services/contact.service";
 
 @Component({
   selector: "app-case-archive",
@@ -23,11 +24,13 @@ export class CaseArchiveComponent implements OnInit {
 
   constructor(
     private caseService: CaseService,
+    private contactService: ContactService,
   ) {
   }
 
   public ngOnInit() {
     this.loadAllCases();
+    this.contactService.hidePrompt();
   }
 
   private loadAllCases() {
