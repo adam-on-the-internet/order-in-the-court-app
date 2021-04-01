320db33408ab8fdd53eb118d4037cdd499caae18
Adam Callanan <a.callanan@sbcglobal.net>
2021-03-31 23:01:18 -0500

    update routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -83,7 +83,7 @@ const routes: Routes = [
 @NgModule({
   exports: [RouterModule],
   declarations: [],
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
 })
 export class AppRoutingModule {
 }
